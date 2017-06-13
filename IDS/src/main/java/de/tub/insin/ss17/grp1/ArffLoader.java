79a2f6261f81ba108f4390b5d57a4c57f3e35e20
Joris Clement <j.clement@campus.tu-berlin.de>
2017-06-13 19:10:00 +0200

    Add prototype ArffLoader, #23.
    probably needs to be reworked.
    structure should stay like this, implementation has to be checked, tested.

--- IDS/src/main/java/de/tub/insin/ss17/grp1/ArffLoader.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/ArffLoader.java
@@ -6,13 +6,41 @@ import weka.classifiers.meta.ClassificationViaRegression;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
 
+// TODO improve this loading operations
 public class ArffLoader{
 
+    private static final String arffFilename = "data.arff";
+
+    private static final String trainFilepath = "./training/" + arffFilename;
+
+    private static final String testFilepath = "./test/" + arffFilename;
+
+    private final File arffFolder;
+
     public ArffLoader(String path) {
+        this.arffFolder = new File(path);
+    }
+
+    public Instances loadTraining() throws Exception {
+        File trainFile = new File(this.arffFolder, trainFilepath);
+        return load(trainFile);
+    }
+
+    public Instances loadTest() throws Exception {
+        File testFile = new File(this.arffFolder, testFilepath);
+        return load(testFile);
+    }
+
+    private Instances load(File file) throws Exception {
+        DataSource source = new DataSource(file.getAbsolutePath());
+        Instances data = source.getDataSet();
+
+        return data;
     }
 }
