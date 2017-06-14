8f3bbb631876807096cc5289b3aa434a5132e722
Joris Clement <j.clement@campus.tu-berlin.de>
2017-06-14 11:38:41 +0200

    Add more test skeletons.

--- IDS/src/test/java/de/tub/insin/ss17/grp1/AppTest.java
+++ IDS/src/test/java/de/tub/insin/ss17/grp1/AppTest.java
@@ -3,6 +3,7 @@ package de.tub.insin.ss17.grp1;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -26,12 +27,29 @@ public class AppTest
         System.out.println("This is executed after each Test");
     }
 
+    // TODO add checks
     @Test
     public void testTrainCommand() {
         String[] argv = {"train",
                          "-f", ARFF_FOLDER,
                          "-p", NN_PARAMS};
         App.main(argv);
+    }
+
+    // TODO add checks
+    @Ignore
+    @Test
+    public void testTestCommand() {
+        String[] argv = {"train,test",
+                         "-f", ARFF_FOLDER,
+                         "-p", NN_PARAMS};
+        App.main(argv);
+    }
+
+    // TODO add test for invalid args
+    @Ignore
+    @Test
+    public void testWrongInput() {
     }
 
 }
