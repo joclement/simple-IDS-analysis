989f35b8aa76be261afb006ebad24e95631abd3a
Philip <wilson@campus.tu-berlin.de>
2017-06-19 19:24:16 +0200

    Adding System.out to view analysis of arff with random algorithm

--- IDS/src/test/java/de/tub/insin/ss17/grp1/TestArffLoader.java
+++ IDS/src/test/java/de/tub/insin/ss17/grp1/TestArffLoader.java
@@ -21,6 +21,7 @@ public class TestArffLoader {
         cvr.buildClassifier(testData);
         Evaluation eval = new Evaluation(testData);
         eval.crossValidateModel(cvr, testData, 15, new Random(1));
+        System.out.println(eval.toSummaryString());
     }
 
 }
