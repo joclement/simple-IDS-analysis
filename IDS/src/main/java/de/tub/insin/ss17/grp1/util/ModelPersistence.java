cf64727b43e0e01edc7fa57347d8ff648197a477
Philipp Christian Nickel <p.nickel@campus.tu-berlin.de>
2017-06-20 15:57:24 +0200

    add load/deserializer

--- IDS/src/main/java/de/tub/insin/ss17/grp1/util/ModelPersistence.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/util/ModelPersistence.java
@@ -1,8 +1,11 @@
 package de.tub.insin.ss17.grp1.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import weka.classifiers.Classifier;
@@ -26,11 +29,37 @@ public class ModelPersistence {
 
         // TODO I think some rework for the try, catch, throw IOException is necessary here for 
         // good closing of the stream
+        try{
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
         oos.writeObject(classifier);
         oos.flush();
         oos.close();
+        System.out.println("Speichern erfolgreich");
+        }catch (IOException e){
+            System.out.println("Speichern nicht erfolgreich");
+        }
     }
 
     // TODO move deserialization here
+    public static void load(File file,
+                            String classifierDescription) 
+                            throws FileNotFoundException, IOException, ClassNotFoundException{
+        // Alle models auflisten
+        File test = new File(MODEL_FOLDER_PATH);
+        
+        String[] dir = test.list();
+        for (int i=0; i > dir.length; i++){
+            System.out.println(dir[i]);
+        }
+        // TODO Auswahl des Users
+        
+        try{
+        ObjectInputStream model = new ObjectInputStream(new FileInputStream(MODEL_FOLDER_PATH + classifierDescription + MODEL_FILE_EXTENSION));
+        
+        Classifier classifier = (Classifier) model.readObject();
+        model.close();
+        }catch (IOException e){
+            System.out.println("Laden nicht erfolgreich");
+        }
+    }
 }
