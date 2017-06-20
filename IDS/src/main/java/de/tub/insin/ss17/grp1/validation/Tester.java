dae626cff9c86abf132280a310ae0efa12f3e44a
Philipp Christian Nickel <p.nickel@campus.tu-berlin.de>
2017-06-20 15:58:10 +0200

    add load, started cross validation

--- IDS/src/main/java/de/tub/insin/ss17/grp1/validation/Tester.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/validation/Tester.java
@@ -1,32 +1,58 @@
 package de.tub.insin.ss17.grp1.validation;
 
 import weka.core.Instances;
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 
+import java.io.File;
+import java.io.IOException;
+
+import de.tub.insin.ss17.grp1.training.MlAlgo;
+import de.tub.insin.ss17.grp1.util.ModelPersistence;
+
 public class Tester {
+    
+    private test classifier;
 
     public Tester() {
         // TODO Auto-generated constructor stub
     }
+    public void load(File folder) throws IOException {
+       // Classifier classifier = ModelPersistence.load(folder, null); 
+        Classifier classifier = this.classifier.getClassifier();
+        ModelPersistence.load(folder, classifierDescription);
+        
+        
+    }
 
-    public void test(Instances instances) {
+    public void test(Instances testingData) {
+        try {
+            this.classifier.test(testingData);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         // TODO Auto-generated method stub
         // TODO include class evaluation and validation
         // TODO load metrics
     }
     
-    public void deseritalizer(){
-        // TODO Auto-generated method stub
-        // TODO Deserialzing training model
+    public void crossValidateModel(Classifier classifier, 
+                                   Instances data, 
+                                   int numFolds, 
+                                   java.util.Random random){
+     // TODO generate cross-validation method
     }
+        
     
-    public void validation(){
-        // TODO generate cross-validation method
+    // TODO Modify 
+    public void save(File folder) throws IOException {
+        Classifier classifier = this.classifier.getClassifier();
+        ModelPersistence.save(classifier, folder, this.classifier.getFilename());
     }
-    
-    public void save(){
         // TODO Auto-generated method stub
-    }
+        // save test-results in folder 
+        
     // TODO Generate Graphs
     // TODO find Graph Library
     
