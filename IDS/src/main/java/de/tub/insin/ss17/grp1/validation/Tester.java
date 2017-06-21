3c41c89a91dfeb632946394e0020483889090869
Joris Clement <j.clement@campus.tu-berlin.de>
2017-06-21 12:26:13 +0200

    Cleanup validation package.
    Remove code for a new start.

--- IDS/src/main/java/de/tub/insin/ss17/grp1/validation/Tester.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/validation/Tester.java
@@ -12,50 +12,11 @@ import de.tub.insin.ss17.grp1.util.ModelPersistence;
 
 public class Tester {
 
-    private test classifier;
-
     public Tester() {
         // TODO Auto-generated constructor stub
     }
-    public void load(File folder) throws IOException {
-       // Classifier classifier = ModelPersistence.load(folder, null); 
-        Classifier classifier = this.classifier.getClassifier();
-        // TODO do not commit non compilable code
-        //ModelPersistence.load(folder, classifierDescription);
 
-
+    public void test(Instances testData) {
     }
-
-    public void test(Instances testingData) {
-        try {
-            this.classifier.test(testingData);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        // TODO Auto-generated method stub
-        // TODO include class evaluation and validation
-        // TODO load metrics
-    }
-
-    public void crossValidateModel(Classifier classifier, 
-                                   Instances data, 
-                                   int numFolds, 
-                                   java.util.Random random){
-     // TODO generate cross-validation method
-    }
-
-
-    // TODO Modify 
-    public void save(File folder) throws IOException {
-        Classifier classifier = this.classifier.getClassifier();
-        ModelPersistence.save(classifier, folder, this.classifier.getFilename());
-    }
-    // TODO Auto-generated method stub
-    // save test-results in folder
-
-    // TODO Generate Graphs
-    // TODO find Graph Library
-
 
 }
