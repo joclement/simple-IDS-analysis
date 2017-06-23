81ac0814148d5579aaba54b2a3b79d4e9bc67786
Philipp Christian Nickel <p.nickel@campus.tu-berlin.de>
2017-06-23 15:40:02 +0200

    Add Debug Logging

--- data-preparation/src/main/java/de/tub/insin/ss17/grp1/CTUManager.java
+++ data-preparation/src/main/java/de/tub/insin/ss17/grp1/CTUManager.java
@@ -5,18 +5,26 @@ import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class CTUManager {
 
     private final File datasetDir;
 
     private final String filename;
 
+    private static final Logger log = LoggerFactory.getLogger(CTUManager.class);
+
     public CTUManager(String datasetDir, String filename){
+        log.debug("-start: CTUManager-");
         this.datasetDir = new File(datasetDir);
         this.filename = filename;
+        log.debug("-finished:CTUManager-");
     }
 
     public List<File> find(List<Integer> scenarios) throws FileNotFoundException {
+        
         List<File> files = new LinkedList<File>();
         for(Integer scenario : scenarios){
             files.add(find(scenario));
@@ -26,11 +34,14 @@ public class CTUManager {
     }
 
     private File find(Integer scenario) throws FileNotFoundException{
+        log.debug("-start: File find-");
         String pathInDataset = scenario.toString() + File.separator + this.filename;
         File file = new File(datasetDir, pathInDataset);
         if(!file.exists()){
+            log.error("ERROR: file not found");
             throw new FileNotFoundException();
         }
+        log.debug("-finished: File find");
         return file;
     }
 
