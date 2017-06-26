21279cf30cbb93b9370e26475e73903234fddba0
Joris Clement <j.clement@campus.tu-berlin.de>
2017-06-26 18:02:07 +0200

    Fix tests broken by commit fcf17aa3.

--- IDS/src/main/java/de/tub/insin/ss17/grp1/util/IDSSharedConstants.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/util/IDSSharedConstants.java
@@ -4,12 +4,10 @@ public class IDSSharedConstants {
 
     public static final int CLASS_COUNT = 3;
 
-    public static final String FLOW = "flow=";
+    public static final String BACKGROUND = "Background";
 
-    public static final String BACKGROUND = FLOW + "Background";
+    public static final String NORMAL = "Normal";
 
-    public static final String NORMAL = FLOW + "Normal";
-
-    public static final String BOTNET = FLOW + "Botnet";
+    public static final String BOTNET = "Botnet";
 
 }
