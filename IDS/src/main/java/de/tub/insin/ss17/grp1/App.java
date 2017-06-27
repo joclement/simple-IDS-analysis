81cd0a2594cb4da4645814d6da9e84ddc25526e3
Joris Clement <j.clement@campus.tu-berlin.de>
2017-06-27 20:08:42 +0200

    Cleanup, correct logging.
    - no need for that much logging information.
    - fix some describing mistakes.

--- IDS/src/main/java/de/tub/insin/ss17/grp1/App.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/App.java
@@ -2,15 +2,24 @@ package de.tub.insin.ss17.grp1;
 
 import com.beust.jcommander.JCommander;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class App
 {
+
+    private final static Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] argv)
     {
+        log.info("--- START ---");
         CliManager cliManager = new CliManager();
         JCommander.newBuilder()
             .addObject(cliManager)
             .build()
             .parse(argv);
         cliManager.run();
+        log.info("--- FINISHED ---");
     }
 }
--- IDS/src/main/java/de/tub/insin/ss17/grp1/training/LinearNNClassifier.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/training/LinearNNClassifier.java
@@ -2,29 +2,20 @@ package de.tub.insin.ss17.grp1.training;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import de.tub.insin.ss17.grp1.util.Param;
 import weka.core.neighboursearch.LinearNNSearch;
 
 
 public class LinearNNClassifier extends NNClassifier {
 
-    private final static Logger log = LoggerFactory.getLogger(LinearNNClassifier.class);
-
     public LinearNNClassifier(List<Param> params) {
         super(params);
-        log.debug("start: LinearNNClassifier");
         this.setSearchAlgo();
-        log.debug("finished: LinearNNClassifier");
     }
 
     private void setSearchAlgo() {
-        log.debug("start: setSearchAlgo");
         LinearNNSearch linearSearch = new LinearNNSearch();
         this.nnClassifier.setNearestNeighbourSearchAlgorithm(linearSearch);
-        log.debug("finished: setSearchAlgo");
     }
 
 }
