e471bcaff28f529ca7b936785c296d56aa3cdd52
Philip_Wilson <philipollaswilson@gmail.com>
2017-07-03 21:46:18 +0200

    Converter to have ports as a numeric attribute

--- data-preparation/src/main/java/de/tub/insin/ss17/grp1/Util.java
+++ data-preparation/src/main/java/de/tub/insin/ss17/grp1/Util.java
@@ -21,4 +21,17 @@ public class Util {
         saveAsArff(data, arffTmp);
         return arffTmp;
     }
+    
+    public static int hex2decimal(String s) {
+    	s = s.substring(2);
+        String digits = "0123456789ABCDEF";
+        s = s.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+        return val;
+    }
 }
