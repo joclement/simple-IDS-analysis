9c9a5e7479ce1379d51c944307cc7512829bee88
Joris Clement <JorisClement@posteo.de>
2017-07-03 15:45:52 +0200

    Reduce redundancy.

--- IDS/src/main/java/de/tub/insin/ss17/grp1/util/IDSSharedConstants.java
+++ IDS/src/main/java/de/tub/insin/ss17/grp1/util/IDSSharedConstants.java
@@ -2,12 +2,17 @@ package de.tub.insin.ss17.grp1.util;
 
 public class IDSSharedConstants {
 
-    public static final int CLASS_COUNT = 3;
-
     public static final String BACKGROUND = "Background";
 
     public static final String NORMAL = "Normal";
 
     public static final String BOTNET = "Botnet";
 
+    public static final String[] CLASS_LABELS_CORRECT_ORDER = {NORMAL, BOTNET, BACKGROUND};
+
+    public static final int CLASS_COUNT;
+
+    static {
+        CLASS_COUNT = CLASS_LABELS_CORRECT_ORDER.length;
+    }
 }
