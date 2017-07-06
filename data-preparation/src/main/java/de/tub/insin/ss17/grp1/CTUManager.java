9e149d65453ae9fdd6b84c3db2294777d5e9840f
Joris Clement <j.clement@campus.tu-berlin.de>
2017-07-07 01:02:40 +0200

    Add sample javadoc.

--- data-preparation/src/main/java/de/tub/insin/ss17/grp1/CTUManager.java
+++ data-preparation/src/main/java/de/tub/insin/ss17/grp1/CTUManager.java
@@ -5,18 +5,38 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
-
+/**
+ * Manager for access to the files in the ctu dataset.
+ *
+ * @author Joris Clement
+ * @author Philip Wilson
+ *
+ */
 public class CTUManager {
 
     private final File datasetDir;
 
     private final String filename;
 
+    /**
+     * Constructs the manager for the ctu dataset.
+     *
+     * @param datasetDir the path to the dataset.
+     * @param filename the name of the scenario csv files,
+     *        the filenames in all scenarios should be the same.
+     */
     public CTUManager(String datasetDir, String filename){
         this.datasetDir = new File(datasetDir);
         this.filename = filename;
     }
 
+    /**
+     * Gets a list of files to the scenarios.
+     *
+     * @param scenarios the number(identifiers) for the scenarios, which should be used.
+     * @return the list of scenario files.
+     * @throws IOException if there is a problem accessing the files.
+     */
     public List<File> find(List<Integer> scenarios) throws IOException {
 
         List<File> files = new LinkedList<File>();
