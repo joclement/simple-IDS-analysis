7fea9acf0399f7ba3d0a26de49e347c15b99a578
Joris Clement <j.clement@campus.tu-berlin.de>
2017-07-13 09:16:30 +0200

    Fix build.

--- data-preparation/src/main/java/de/tub/insin/ss17/grp1/CSV2ArffConverter.java
+++ data-preparation/src/main/java/de/tub/insin/ss17/grp1/CSV2ArffConverter.java
@@ -21,6 +21,7 @@ import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.tub.insin.ss17.grp1.shared.DataSharedConstants;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
