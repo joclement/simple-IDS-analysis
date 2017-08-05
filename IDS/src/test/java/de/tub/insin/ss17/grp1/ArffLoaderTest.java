28f3e03595c17691b324c5a75bf5c85c13e0e3bb
Joris Clement <j.clement@campus.tu-berlin.de>
2017-08-05 13:21:46 +0200

    Remove unnecessary try catches.

--- IDS/src/test/java/de/tub/insin/ss17/grp1/ArffLoaderTest.java
+++ IDS/src/test/java/de/tub/insin/ss17/grp1/ArffLoaderTest.java
@@ -33,41 +33,29 @@ public class ArffLoaderTest {
     @Test
     public void testBasic() {
         ArffLoader loader = new ArffLoader(TestHelper.ARFF_FOLDER);
-        try {
-            Instances testData = loader.loadTest();
-            assertTrue(testData.classIndex() >= 0);
-            assertTrue(testData.numClasses() >= 0);
-            assertTrue(testData.attribute(IP_SRC_IDX).isNumeric());
-            assertTrue(testData.attribute(PORT_SRC_IDX).isNumeric());
-            assertTrue(testData.attribute(IP_DEST_IDX).isNumeric());
-            assertTrue(testData.attribute(PORT_DEST_IDX).isNumeric());
-            assertTrue(testData.attribute(TOS_IDX).isNumeric());
-            assertTrue(testData.attribute(TOS_DEST_IDX).isNumeric());
-            Instances trainingData = loader.loadTraining();
-            assertTrue(trainingData.classIndex() >= 0);
-            assertTrue(trainingData.numClasses() >= 0);
-            assertTrue(trainingData.attribute(IP_SRC_IDX).isNumeric());
-            assertTrue(trainingData.attribute(IP_DEST_IDX).isNumeric());
-            assertTrue(trainingData.attribute(TOS_IDX).isNumeric());
-            assertTrue(trainingData.attribute(TOS_DEST_IDX).isNumeric());
-        } catch (Exception e) {
-            assertTrue(false);
-            e.printStackTrace();
-        }
+        Instances testData = loader.loadTest();
+        assertTrue(testData.classIndex() >= 0);
+        assertTrue(testData.numClasses() >= 0);
+        assertTrue(testData.attribute(IP_SRC_IDX).isNumeric());
+        assertTrue(testData.attribute(PORT_SRC_IDX).isNumeric());
+        assertTrue(testData.attribute(IP_DEST_IDX).isNumeric());
+        assertTrue(testData.attribute(PORT_DEST_IDX).isNumeric());
+        assertTrue(testData.attribute(TOS_IDX).isNumeric());
+        assertTrue(testData.attribute(TOS_DEST_IDX).isNumeric());
+        Instances trainingData = loader.loadTraining();
+        assertTrue(trainingData.classIndex() >= 0);
+        assertTrue(trainingData.numClasses() >= 0);
+        assertTrue(trainingData.attribute(IP_SRC_IDX).isNumeric());
+        assertTrue(trainingData.attribute(IP_DEST_IDX).isNumeric());
+        assertTrue(trainingData.attribute(TOS_IDX).isNumeric());
+        assertTrue(trainingData.attribute(TOS_DEST_IDX).isNumeric());
     }
 
     @Test
     public void testCorrectSeperationIntoTrainingAndTest() {
         ArffLoader loader = new ArffLoader(TestHelper.ARFF_FOLDER);
-        Instances training = null;
-        Instances test = null;
-        try {
-            training = loader.loadTraining();
-            test = loader.loadTest();
-        } catch (Exception e) {
-            assertTrue(false);
-            e.printStackTrace();
-        }
+        Instances training = loader.loadTraining();
+        Instances test = loader.loadTest();
 
         for (Instance trainInstance : training) {
             for (Instance testInstance : test) {
@@ -79,25 +67,21 @@ public class ArffLoaderTest {
     @Test
     public void testNumericToNominalConversion() {
         ArffLoader loader = new ArffLoader(TestHelper.ARFF_FOLDER, NUMERIC_TO_NOMINAL_COLS);
-        try {
-            Instances trainingData = loader.loadTraining();
-            assertTrue(trainingData.attribute(IP_SRC_IDX).isNominal());
-            assertTrue(trainingData.attribute(PORT_SRC_IDX).isNumeric());
-            assertTrue(trainingData.attribute(IP_DEST_IDX).isNominal());
-            assertTrue(trainingData.attribute(PORT_DEST_IDX).isNumeric());
-            assertTrue(trainingData.attribute(TOS_IDX).isNominal());
-            assertTrue(trainingData.attribute(TOS_DEST_IDX).isNominal());
 
-            Instances testData = loader.loadTest();
-            assertTrue(testData.attribute(IP_SRC_IDX).isNominal());
-            assertTrue(testData.attribute(PORT_SRC_IDX).isNumeric());
-            assertTrue(testData.attribute(IP_DEST_IDX).isNominal());
-            assertTrue(testData.attribute(PORT_DEST_IDX).isNumeric());
-            assertTrue(testData.attribute(TOS_IDX).isNominal());
-            assertTrue(testData.attribute(TOS_DEST_IDX).isNominal());
-        } catch (Exception e) {
-            assertTrue(false);
-            e.printStackTrace();
-        }
+        Instances trainingData = loader.loadTraining();
+        assertTrue(trainingData.attribute(IP_SRC_IDX).isNominal());
+        assertTrue(trainingData.attribute(PORT_SRC_IDX).isNumeric());
+        assertTrue(trainingData.attribute(IP_DEST_IDX).isNominal());
+        assertTrue(trainingData.attribute(PORT_DEST_IDX).isNumeric());
+        assertTrue(trainingData.attribute(TOS_IDX).isNominal());
+        assertTrue(trainingData.attribute(TOS_DEST_IDX).isNominal());
+
+        Instances testData = loader.loadTest();
+        assertTrue(testData.attribute(IP_SRC_IDX).isNominal());
+        assertTrue(testData.attribute(PORT_SRC_IDX).isNumeric());
+        assertTrue(testData.attribute(IP_DEST_IDX).isNominal());
+        assertTrue(testData.attribute(PORT_DEST_IDX).isNumeric());
+        assertTrue(testData.attribute(TOS_IDX).isNominal());
+        assertTrue(testData.attribute(TOS_DEST_IDX).isNominal());
     }
 }
