92f3e5337a7c1edbf0de5cdaa88846bda449d568
Joris Clement <j.clement@campus.tu-berlin.de>
2017-08-05 13:01:28 +0200

    Add options for conversion of numeric to nominal, #63.
    This option gives the user the ability to specify columns in the arff
    data-set, which should be converted to nominal. This would make sense
    f.x. for ip addresses. These are currently in our arff files stored as
    numeric. But their nature is more of a nominal type.

--- IDS/src/test/java/de/tub/insin/ss17/grp1/ArffLoaderTest.java
+++ IDS/src/test/java/de/tub/insin/ss17/grp1/ArffLoaderTest.java
@@ -13,6 +13,18 @@ import weka.core.Instances;
 
 public class ArffLoaderTest {
 
+    private final static int IP_SRC_IDX = 3;
+    private final static int PORT_SRC_IDX = 4;
+    private final static int IP_DEST_IDX = 6;
+    private final static int PORT_DEST_IDX = 7;
+    private final static int TOS_IDX = 9;
+    private final static int TOS_DEST_IDX = 10;
+
+    protected final static String NUMERIC_TO_NOMINAL_COLS = IP_SRC_IDX   + "," +
+                                                            IP_DEST_IDX  + "," +
+                                                            TOS_IDX      + "," +
+                                                            TOS_DEST_IDX + ",";
+
     @Before
     public void beforeEachTest() {
         TestHelper.assertArff();
@@ -25,9 +37,19 @@ public class ArffLoaderTest {
             Instances testData = loader.loadTest();
             assertTrue(testData.classIndex() >= 0);
             assertTrue(testData.numClasses() >= 0);
+            assertTrue(testData.attribute(IP_SRC_IDX).isNumeric());
+            assertTrue(testData.attribute(PORT_SRC_IDX).isNumeric());
+            assertTrue(testData.attribute(IP_DEST_IDX).isNumeric());
+            assertTrue(testData.attribute(PORT_DEST_IDX).isNumeric());
+            assertTrue(testData.attribute(TOS_IDX).isNumeric());
+            assertTrue(testData.attribute(TOS_DEST_IDX).isNumeric());
             Instances trainingData = loader.loadTraining();
             assertTrue(trainingData.classIndex() >= 0);
             assertTrue(trainingData.numClasses() >= 0);
+            assertTrue(trainingData.attribute(IP_SRC_IDX).isNumeric());
+            assertTrue(trainingData.attribute(IP_DEST_IDX).isNumeric());
+            assertTrue(trainingData.attribute(TOS_IDX).isNumeric());
+            assertTrue(trainingData.attribute(TOS_DEST_IDX).isNumeric());
         } catch (Exception e) {
             assertTrue(false);
             e.printStackTrace();
@@ -51,6 +73,31 @@ public class ArffLoaderTest {
             for (Instance testInstance : test) {
                 assertFalse(trainInstance.equals(testInstance));
             }
+        }
+    }
+
+    @Test
+    public void testNumericToNominalConversion() {
+        ArffLoader loader = new ArffLoader(TestHelper.ARFF_FOLDER, NUMERIC_TO_NOMINAL_COLS);
+        try {
+            Instances trainingData = loader.loadTraining();
+            assertTrue(trainingData.attribute(IP_SRC_IDX).isNominal());
+            assertTrue(trainingData.attribute(PORT_SRC_IDX).isNumeric());
+            assertTrue(trainingData.attribute(IP_DEST_IDX).isNominal());
+            assertTrue(trainingData.attribute(PORT_DEST_IDX).isNumeric());
+            assertTrue(trainingData.attribute(TOS_IDX).isNominal());
+            assertTrue(trainingData.attribute(TOS_DEST_IDX).isNominal());
+
+            Instances testData = loader.loadTest();
+            assertTrue(testData.attribute(IP_SRC_IDX).isNominal());
+            assertTrue(testData.attribute(PORT_SRC_IDX).isNumeric());
+            assertTrue(testData.attribute(IP_DEST_IDX).isNominal());
+            assertTrue(testData.attribute(PORT_DEST_IDX).isNumeric());
+            assertTrue(testData.attribute(TOS_IDX).isNominal());
+            assertTrue(testData.attribute(TOS_DEST_IDX).isNominal());
+        } catch (Exception e) {
+            assertTrue(false);
+            e.printStackTrace();
         }
     }
 }
