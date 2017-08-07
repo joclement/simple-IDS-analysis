8b3c124e2da57f26d869dc971692d3b1511f71e9
Joris Clement <j.clement@campus.tu-berlin.de>
2017-08-07 15:55:04 +0200

    Improve conversion, fix bad boxing unboxing.

--- data-preparation/src/main/java/de/tub/insin/ss17/grp1/shared/SharedUtil.java
+++ data-preparation/src/main/java/de/tub/insin/ss17/grp1/shared/SharedUtil.java
@@ -19,7 +19,7 @@ public class SharedUtil {
     }
 
     public static int checkedConvert(double num) {
-        int converted = new Double(num).intValue();
+        int converted = (int) num;
         if (converted != num) {
             throw new NumberFormatException("Number conversion error.");
         }
